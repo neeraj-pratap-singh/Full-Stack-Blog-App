fc071acd745e3826d463ffc5d03f8d671e0ed2a9
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2024-03-07 23:03:09 +0530

    updated layout and global css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b8e226039885b6e3"; }
+/******/ 		__webpack_require__.h = function() { return "b69207d633f4f6f6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
