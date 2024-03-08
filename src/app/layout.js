1e6111d21cf80c2674999a2e9b6ff090b5dc2a84
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2024-03-08 10:14:13 +0530

    added navbar and footer component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import NavBar from "@/components/navbar/NavBar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,11 +14,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-              <div className="container">
-                <div className="wrapper">
-                  {children}
-                </div>
-              </div>
+        <div className="container">
+          <div className="wrapper">
+            <NavBar />
+            {children}
+            <Footer />
+          </div>
+        </div>
       </body>
     </html>
   );
