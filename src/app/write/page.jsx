f62d6b4fab4add0affb6237441397a9623b62fa0
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2024-03-10 16:34:17 +0530

    removed github login

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import styles from "./writePage.module.css";
 import { useEffect, useState } from "react";
-import "react-quill/dist/quill.bubble.css";
+// import "react-quill/dist/quill.bubble.css";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import {
@@ -13,10 +13,12 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 import { app } from "@/utils/firebase";
-import ReactQuill from "react-quill";
+// import ReactQuill from "react-quill";
+import dynamic from "next/dynamic";
 
 const WritePage = () => {
   const { status } = useSession();
+  const ReactQuill = dynamic(() => import('react-quill'), {ssr: false})
   const router = useRouter();
 
   const [open, setOpen] = useState(false);
