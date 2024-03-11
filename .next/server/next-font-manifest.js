1a5c5e8b8a58b338a562f43437f696dbfb5d762a
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2024-03-11 07:24:52 +0530

    updated write style

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/zero2one/Documents/Vlearn hero/Full-Stack-Blog-App/src/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/zero2one/Documents/Vlearn hero/Full-Stack-Blog-App/src/app/layout":["static/media/c9a5bc6a7c948fb0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
